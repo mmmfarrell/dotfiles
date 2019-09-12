815b5d727b7dff1d5d69ada369d34e4cd6f41bb6
Michael Farrell <michaeldavidfarrell@gmail.com>
2019-09-12 11:52:35 -0600

    change display scripts

---
+++ .config/i3/my_lock.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+B='#00000000'  # blank
+C='#ffffff22'  # clear ish
+D='#ff00ffcc'  # default
+T='#ee00eeee'  # text
+W='#880000bb'  # wrong
+V='#bb00bbbb'  # verifying
+
+BL='#0000ffcc'  # Blue
+RE='#ff0000cc'  # Red
+WH='#ffffffcc'  # White
+YE='#ffff00cc'  # White
+
+i3lock \
+--insidevercolor=$C   \
+--ringvercolor=$YE     \
+\
+--insidewrongcolor=$C \
+--ringwrongcolor=$RE   \
+\
+--insidecolor=$B      \
+--ringcolor=$BL        \
+--linecolor=$B        \
+\
+--verifcolor=$YE        \
+--wrongcolor=$RE        \
+--timecolor=$WH        \
+--datecolor=$WH        \
+--layoutcolor=$T      \
+--keyhlcolor=$WH       \
+--bshlcolor=$WH        \
+--separatorcolor=$WH   \
+\
+--blur 5              \
+--clock               \
+--indicator           \
+--timestr="%I:%M:%S"  \
+--datestr="%D" \
+\
+--radius=90 \
+--ring-width=9 \
+#-i $HOME/Pictures/wallpaper.png
+#--screen 1            \
+#--timestr="%H:%M:%S"  \
+#--datestr="%A, %m %Y" \
+#--keylayout 2         \
+
+# --veriftext="Drinking verification can..."
+# --wrongtext="Nope!"
+# --textsize=20
+# --modsize=10
+# --timefont=comic-sans
+# --datefont=monofur
+# etc
